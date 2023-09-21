88385d647821f5a7bcd9e85a55f42fe288f24ba2
Samir Dzhafari <samirjafari2001@icloud.com>
2023-09-21 21:07:15 +0300

    Initial Commit

---
+++ HW 2.2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW 2.2
+//
+//  Created by Самир Джафари on 21.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
